458438b93d7ff515d4779298b0292b30b977696c
dgsglryz <dogusguleryuz@gmail.com>
2025-11-15 03:00:51 -0800

    fix(inertia): load module page assets in layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Scripts -->
         @routes
-        @vite('resources/js/app.ts')
+        @vite(['resources/js/app.ts', "resources/js/Modules/Core/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
