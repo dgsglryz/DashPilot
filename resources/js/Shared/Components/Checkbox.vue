8dc133d13dd02e3cc7b784c3bbb0cdc4286248f7
dgsglryz <dogusguleryuz@gmail.com>
2025-11-16 17:20:35 -0800

    fix(sonar): associate form label with checkbox control in Login.vue

--- resources/js/Shared/Components/Checkbox.vue
+++ resources/js/Shared/Components/Checkbox.vue
@@ -6,6 +6,8 @@ const emit = defineEmits(['update:checked']);
 const props = defineProps<{
     checked: boolean;
     value?: string | number | boolean;
+    id?: string;
+    name?: string;
 }>();
 
 const proxyChecked = computed({
@@ -22,6 +24,8 @@ const proxyChecked = computed({
 <template>
     <input
         type="checkbox"
+        :id="id"
+        :name="name"
         :value="value"
         v-model="proxyChecked"
         class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
